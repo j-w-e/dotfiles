54cd76db38d6d1a798d8f42a8fc89fc755c28aad
Hugh Earp <hugh.earp@gmail.com>
2022-07-31 18:23:16 +0200

    working, with lsp disabled

--- nvim/init.lua
+++ nvim/init.lua
@@ -44,19 +44,21 @@ end
 packer.startup(function(use)
     use 'wbthomason/packer.nvim'           -- packer manages itself
     use 'nvim-lua/plenary.nvim'            -- avoids callbacks, used by other plugins
-    --use 'nvim-lua/popup.nvim'              -- popup for other plugins
     use 'nvim-treesitter/nvim-treesitter'  -- language parsing completion engine
-    use 'williamboman/nvim-lsp-installer'  -- UI for fetching/downloading LSPs
-    use 'neovim/nvim-lspconfig'            -- language server protocol implementation
+    --use 'williamboman/nvim-lsp-installer'  -- UI for fetching/downloading LSPs
+    --use 'neovim/nvim-lspconfig'            -- language server protocol implementation
+
+    --use 'nvim-lua/popup.nvim'              -- popup for other plugins
     --use 'hrsh7th/nvim-cmp'                 -- THE vim completion engine
     --use 'L3MON4D3/LuaSnip'                 -- more snippets
     --use 'saadparwaiz1/cmp_luasnip'         -- even more snippets
     --use 'hrsh7th/cmp-nvim-lsp'
     --use 'hrsh7th/cmp-buffer'
     --use 'hrsh7th/cmp-path'
+    --use 'jose-elias-alvarez/null-ls.nvim' -- see https://www.youtube.com/watch?v=b7OguLuaYvE
+
     use 'nvim-telescope/telescope.nvim'    -- finder, requires fzf and ripgrep
     use 'kyazdani42/nvim-tree.lua'
-    --use 'jose-elias-alvarez/null-ls.nvim' -- see https://www.youtube.com/watch?v=b7OguLuaYvE
     use 'jalvesaq/Nvim-R'
     use 'kyazdani42/nvim-web-devicons'
     use 'lewis6991/gitsigns.nvim'
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -84,16 +84,6 @@ _G.packer_plugins = {
     path = "/Users/hughearp/.local/share/nvim/site/pack/packer/start/mini.nvim",
     url = "https://github.com/echasnovski/mini.nvim"
   },
-  ["nvim-lsp-installer"] = {
-    loaded = true,
-    path = "/Users/hughearp/.local/share/nvim/site/pack/packer/start/nvim-lsp-installer",
-    url = "https://github.com/williamboman/nvim-lsp-installer"
-  },
-  ["nvim-lspconfig"] = {
-    loaded = true,
-    path = "/Users/hughearp/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
-    url = "https://github.com/neovim/nvim-lspconfig"
-  },
   ["nvim-tree.lua"] = {
     loaded = true,
     path = "/Users/hughearp/.local/share/nvim/site/pack/packer/start/nvim-tree.lua",
