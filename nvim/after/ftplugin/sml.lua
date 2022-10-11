7394edefa4ef43635e1e825016c51fc7d520c13c
Hugh Earp <hugh.earp@gmail.com>
2022-10-11 18:55:36 +0200

    added sml custom after plugin

---
+++ nvim/after/ftplugin/sml.lua
@@ -0,0 +1,4 @@
+-- local keymap = vim.api.nvim_set_keymap
+-- vim.api.nvim_buf_set_keymap(0, "n", "<leader>rf", "<cmd>IronRepl<cr>", {desc = "start repl"})
+
+-- vim.api.nvim_buf_set_keymap(0, "n", "<leader>d", "<cmd>lua require(\"iron.core\").send_line()<cr>j", {desc = "send line and down"})
