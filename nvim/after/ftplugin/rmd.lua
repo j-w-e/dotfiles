94822b4afc6fd67f9fa09a8d8904382b5d2a5d51
Hugh Earp <hugh.earp@gmail.com>
2025-02-10 07:47:19 +0100

    Stylua formating changes

--- nvim/after/ftplugin/rmd.lua
+++ nvim/after/ftplugin/rmd.lua
@@ -4,7 +4,7 @@ vim.opt_local.sw = 2
 vim.opt_local.wrap = true
 -- vim.opt_local.spell = true
 vim.opt.conceallevel = 0
-vim.b.miniindentscope_config = { options = { border = "top" } }
+vim.b.miniindentscope_config = { options = { border = 'top' } }
 
 -- Remove the backtick from pairing, since this should be taken care of by r.nvim
-vim.keymap.set("i", "`", "<Cmd>lua require('r.rmd').write_chunk()<CR>", { buffer = 0 })
+-- vim.keymap.set("i", "`", "<Cmd>lua require('r.rmd').write_chunk()<CR>", { buffer = 0 })
--- nvim/lua/plugins/r.lua
+++ nvim/lua/plugins/r.lua
@@ -35,81 +35,75 @@ return {
   -- },
 
   {
-    "R-nvim/R.nvim",
+    'R-nvim/R.nvim',
     config = function()
       -- Create a table with the options to be passed to setup()
       local opts = {
-        R_args = { "--quiet", "--no-save" },
+        R_args = { '--quiet', '--no-save' },
         hook = {
           on_filetype = function()
             -- This function will be called at the FileType event
             -- of files supported by R.nvim. This is an
             -- opportunity to create mappings local to buffers.
-            vim.api.nvim_buf_set_keymap(0, "n", "<Enter>", "<Plug>RDSendLine", {})
-            vim.api.nvim_buf_set_keymap(0, "v", "<Enter>", "<Plug>RSendSelection", {})
-            vim.api.nvim_buf_set_keymap(0, "n", "<localleader>rr", "<cmd>RMapsDesc<cr>", {})
-            vim.api.nvim_buf_set_keymap(0, "n", "<localleader>rx", "<Plug>RClose", {})
-            vim.api.nvim_buf_set_keymap(0, "i", "%%", " %>%", {})
+            vim.api.nvim_buf_set_keymap(0, 'n', '<Enter>', '<Plug>RDSendLine', {})
+            vim.api.nvim_buf_set_keymap(0, 'v', '<Enter>', '<Plug>RSendSelection', {})
+            vim.api.nvim_buf_set_keymap(0, 'n', '<localleader>rr', '<cmd>RMapsDesc<cr>', {})
+            vim.api.nvim_buf_set_keymap(0, 'n', '<localleader>rx', '<Plug>RClose', {})
+            vim.api.nvim_buf_set_keymap(0, 'i', '%%', ' %>%', {})
             -- vim.api.nvim_buf_set_keymap(0, 'i', '<c-,>', ' |>', {})
-            vim.api.nvim_buf_set_keymap(0, "n", "<localleader><Enter>", "<Plug>RSendLine", {})
-            vim.api.nvim_buf_set_keymap(0, "n", "<localleader>b", "<Plug>RPreviousRChunk", {})
-            vim.api.nvim_buf_set_keymap(0, "n", "<localleader>n", "<Plug>RNextRChunk", {})
-            vim.api.nvim_buf_set_keymap(0, "n", "<localleader>h", "<Plug>RHelp", {})
-            vim.api.nvim_buf_set_keymap(
-              0,
-              "n",
-              "<LocalLeader>rh",
-              "<cmd>lua require('r.run').action('head', 'n', ', n = 15')<cr>",
-              {}
-            )
-            vim.api.nvim_buf_set_keymap(0, "n", "<LocalLeader>kx", "<cmd>lua require('r.rmd').make('pptx')<cr>", {})
+            vim.api.nvim_buf_set_keymap(0, 'n', '<localleader><Enter>', '<Plug>RSendLine', {})
+            vim.api.nvim_buf_set_keymap(0, 'n', '<localleader>b', '<Plug>RPreviousRChunk', {})
+            vim.api.nvim_buf_set_keymap(0, 'n', '<localleader>n', '<Plug>RNextRChunk', {})
+            vim.api.nvim_buf_set_keymap(0, 'n', '<localleader>h', '<Plug>RHelp', {})
+            vim.api.nvim_buf_set_keymap(0, 'n', '<LocalLeader>rh', "<cmd>lua require('r.run').action('head', 'n', ', n = 15')<cr>", {})
+            vim.api.nvim_buf_set_keymap(0, 'n', '<LocalLeader>kx', "<cmd>lua require('r.rmd').make('pptx')<cr>", {})
 
-            require("hydra")({
-              name = "R",
+            require 'hydra' {
+              name = 'R',
               hint = [[ Scroll or send-to-R ]],
-              mode = "n",
-              body = "<localleader>",
+              mode = 'n',
+              body = '<localleader>',
               heads = {
-                { "n", "<Plug>RNextRChunk", { desc = "Next R chunk" } },
-                { "b", "<Plug>RPreviousRChunk", { desc = "Prev R chunk" } },
-                { "cd", "<Plug>RDSendChunk", { exit = true, desc = "Run chunk and go to next" } },
-                { "cc", "<Plug>RSendChunk", { exit = true, desc = "Run chunk" } },
+                { 'n', '<Plug>RNextRChunk', { desc = 'Next R chunk' } },
+                { 'b', '<Plug>RPreviousRChunk', { desc = 'Prev R chunk' } },
+                { 'cd', '<Plug>RDSendChunk', { exit = true, desc = 'Run chunk and go to next' } },
+                { 'cc', '<Plug>RSendChunk', { exit = true, desc = 'Run chunk' } },
               },
               config = {
                 buffer = true,
               },
-            })
+            }
           end,
         },
         min_editor_width = 72,
         rconsole_width = 78,
         rm_knit_cache = true,
-        -- rmdchunk = "`",
+        rmdchunk = '',
         disable_cmds = {
-          "RClearConsole",
-          "RCustomStart",
-          "RSPlot",
-          "RSaveClose",
-          "RDSendMBlock",
-          "RSendMBlock",
+          'RClearConsole',
+          'RCustomStart',
+          'RSPlot',
+          'RSaveClose',
+          'RDSendMBlock',
+          'RSendMBlock',
         },
-        assignment_keymap = "<c-->",
-        pipe_keymap = "<space><space>",
+        assignment_keymap = '<c-->',
+        pipe_keymap = '<space><space>',
         -- pipe_keymap = '<c-,>',
-        pdfviewer = "open",
+        pdfviewer = 'open',
       }
       -- Check if the environment variable "R_AUTO_START" exists.
       -- this is in my .zshrc: alias r="R_AUTO_START=true nvim"
-      if vim.env.R_AUTO_START == "true" then
-        opts.auto_start = "on startup"
+      if vim.env.R_AUTO_START == 'true' then
+        opts.auto_start = 'on startup'
         -- opts.objbr_auto_start = true
       end
-      require("r").setup(opts)
+      require('r').setup(opts)
     end,
     lazy = false,
   },
 
-  { "R-nvim/cmp-r" },
+  { 'R-nvim/cmp-r' },
 
   -- {
   --   'vhyrro/luarocks.nvim',
