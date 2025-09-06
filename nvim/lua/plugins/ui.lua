ca0c4099d2567813cda62b797da3557f0713ee9b
Hugh Earp <hugh.earp@gmail.com>
2025-09-06 15:33:56 +0200

    added hydra

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,40 +1,40 @@
 return {
-  { 'meznaric/key-analyzer.nvim', opts = {} },
-  { 'numToStr/FTerm.nvim', opts = {} },
+  { "meznaric/key-analyzer.nvim", opts = {} },
+  { "numToStr/FTerm.nvim", opts = {} },
   {
-    'chrishrb/gx.nvim',
-    keys = { { 'gx', '<cmd>Browse<cr>', mode = { 'n', 'x' } } },
-    cmd = { 'Browse' },
+    "chrishrb/gx.nvim",
+    keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+    cmd = { "Browse" },
     init = function()
       vim.g.netrw_nogx = 1 -- disable netrw gx
     end,
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { "nvim-lua/plenary.nvim" },
     submodules = false, -- not needed, submodules are required only for tests
     opts = {
       handler_options = {
         -- you can select between google, bing, duckduckgo, and ecosia
-        search_engine = 'duckduckgo',
+        search_engine = "duckduckgo",
       },
     },
   },
 
   {
-    'folke/flash.nvim',
-    event = 'VeryLazy',
+    "folke/flash.nvim",
+    event = "VeryLazy",
     ---@type Flash.Config
     opts = {
       -- labels = "abcdefghijklmnopqrstuvwxyz",
       -- labels = "enaiorsthwfpluaycgdm",
-      labels = 'enaiohtsrluypfwmdgc',
+      labels = "enaiohtsrluypfwmdgc",
       search = {
-        mode = 'search',
+        mode = "search",
       },
       modes = {
         char = {
           char_actions = function()
             return {
-              [';'] = 'prev', -- set to `right` to always go right
-              [','] = 'next', -- set to `left` to always go left
+              [";"] = "prev", -- set to `right` to always go right
+              [","] = "next", -- set to `left` to always go left
             }
           end,
         },
@@ -51,15 +51,15 @@ return {
   },
 
   {
-    'bullets-vim/bullets.vim',
+    "bullets-vim/bullets.vim",
     config = function()
-      vim.cmd [[let g:bullets_delete_last_bullet_if_empty = 2]]
+      vim.cmd([[let g:bullets_delete_last_bullet_if_empty = 2]])
     end,
     ft = {
-      'markdown',
-      'telekasten',
-      'text',
-      'scratch',
+      "markdown",
+      "telekasten",
+      "text",
+      "scratch",
     },
   },
 
@@ -76,12 +76,12 @@ return {
   -- },
 
   {
-    'nguyenvukhang/nvim-toggler',
-    opts = { inverses = { ['TRUE'] = 'FALSE' } },
+    "nguyenvukhang/nvim-toggler",
+    opts = { inverses = { ["TRUE"] = "FALSE" } },
   },
 
   {
-    'folke/noice.nvim',
+    "folke/noice.nvim",
     -- event = "VeryLazy",
     opts = {
       presets = {
@@ -91,24 +91,24 @@ return {
       },
     },
     dependencies = {
-      'MunifTanjim/nui.nvim',
-      'rcarriga/nvim-notify',
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
     },
   },
 
   {
-    'samjwill/nvim-unception',
+    "samjwill/nvim-unception",
     config = function()
       vim.g.unception_open_buffer_in_new_tab = true
     end,
   },
 
-  { 'tzachar/highlight-undo.nvim', opts = {} },
+  { "tzachar/highlight-undo.nvim", opts = {} },
 
-  { 'romainl/vim-cool' },
+  { "romainl/vim-cool" },
 
   {
-    'folke/zen-mode.nvim',
+    "folke/zen-mode.nvim",
     opts = {
       window = {
         width = 150,
@@ -117,57 +117,74 @@ return {
   },
 
   {
-    'nvimtools/hydra.nvim',
+    "nvimtools/hydra.nvim",
     config = function()
-      local hydra = require 'hydra'
-      require('hydra').setup {
+      local hydra = require("hydra")
+      require("hydra").setup({
         timeout = 4000,
-      }
+      })
 
-      hydra {
-        name = 'Side scroll',
+      hydra({
+        name = "Side scroll",
         hint = [[ Side scroll ]],
-        mode = 'n',
-        body = 'z',
+        mode = "n",
+        body = "z",
         heads = {
-          { 'h', '5zh' },
-          { 'l', '5zl', { desc = '←/→' } },
-          { 'H', 'zH' },
-          { 'L', 'zL', { desc = 'half screen ←/→' } },
+          { "h", "5zh" },
+          { "l", "5zl", { desc = "←/→" } },
+          { "H", "zH" },
+          { "L", "zL", { desc = "half screen ←/→" } },
         },
-      }
+      })
 
-      hydra {
-        name = 'Buffers',
+      hydra({
+        name = "Buffers",
         hint = [[Buffers]],
-        mode = 'n',
-        body = 't',
-        config = { color = 'pink' },
+        mode = "n",
+        body = "t",
+        config = { color = "pink" },
         heads = {
           {
-            'h',
+            "h",
             function()
-              vim.cmd 'bprev'
+              vim.cmd("bprev")
             end,
+            { desc = "Prev buffer" },
           },
           {
-            'l',
+            "l",
             function()
-              vim.cmd 'bnext'
+              vim.cmd("bnext")
             end,
+            { desc = "Next buffer" },
           },
           {
-            'x',
+            "x",
             function()
               MiniBufremove.delete(0)
             end,
+            { desc = "Close buffer" },
           },
-          { '<Esc>', nil, { exit = true } },
+          {
+            "n",
+            function()
+              JumpToTODO(true)
+            end,
+            { desc = "jump to next todo" },
+          },
+          {
+            "p",
+            function()
+              JumpToTODO(false)
+            end,
+            { desc = "jump to prev todo" },
+          },
+          { "<Esc>", nil, { exit = true } },
         },
-      }
+      })
 
-      hydra {
-        name = 'Windows',
+      hydra({
+        name = "Windows",
         hint = [[Windows]],
         -- config = {
         --   invoke_on_body = true,
@@ -176,23 +193,23 @@ return {
         --     offset = -1,
         --   },
         -- },
-        mode = 'n',
-        body = '<C-w>',
+        mode = "n",
+        body = "<C-w>",
         heads = {
-          { 'h', '<cmd>vertical resize -20<cr>', { desc = 'resize -20' } },
-          { 'l', '<cmd>vertical resize +20<cr>', { desc = 'resize +20' } },
+          { "h", "<cmd>vertical resize -20<cr>", { desc = "resize -20" } },
+          { "l", "<cmd>vertical resize +20<cr>", { desc = "resize +20" } },
 
-          { '=', '<C-w>=', { desc = 'equalize' } },
+          { "=", "<C-w>=", { desc = "equalize" } },
 
-          { 's', '<cmd>sp<cr>', { desc = 'new horz split' } },
-          { 'v', '<cmd>vs<cr>', { desc = 'new vert split' } },
+          { "s", "<cmd>sp<cr>", { desc = "new horz split" } },
+          { "v", "<cmd>vs<cr>", { desc = "new vert split" } },
 
-          { 'w', '<C-w>w', { exit = true, desc = false } },
-          { '<C-w>', '<C-w>w', { exit = true, desc = false } },
+          { "w", "<C-w>w", { exit = true, desc = false } },
+          { "<C-w>", "<C-w>w", { exit = true, desc = false } },
 
-          { '<Esc>', nil, { exit = true, desc = false } },
+          { "<Esc>", nil, { exit = true, desc = false } },
         },
-      }
+      })
 
       -- hydra {
       --   name = 'R chunks',
@@ -252,17 +269,17 @@ return {
   -- },
   -- { 'bluz71/vim-nightfly-colors', name = 'nightfly', lazy = false, priority = 1000 },
   {
-    'folke/tokyonight.nvim',
+    "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
     opts = {
-      style = 'storm',
+      style = "storm",
       on_highlights = function(highlights, colors)
         highlights.FlashLabel = { bg = colors.blue0, fg = colors.magenta }
         highlights.MiniTrailspace = { fg = colors.magenta }
         highlights.CursorLine = { bg = colors.fg_gutter }
         -- highlights.RenderMarkdownCode = { bg = colors.fg_gutter }  -- this is a lighter backgroud for code blocks. I got tired of it
-        highlights.RenderMarkdownCode = { bg = '#16161e' } -- this is for a dark background to code blocks
+        highlights.RenderMarkdownCode = { bg = "#16161e" } -- this is for a dark background to code blocks
       end,
     },
   },
